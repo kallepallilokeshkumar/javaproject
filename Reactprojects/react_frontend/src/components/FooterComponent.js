527533c00f7ca379ab06aaa88ba1227b29417a1f
kallepallilokeshkumar <kallepallilokeshkumar@gmail.com>
2024-05-12 01:47:50 +0530

    project added

---
+++ Reactprojects/react_frontend/src/components/FooterComponent.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import '../App.css';
+
+export default class FooterComponent extends Component {
+  render() {
+    return (
+      <div>
+        <footer className="footer">
+            <span>All Rights Reserved &copy; by Niyaz Sir</span>
+        </footer>
+      </div>
+    )
+  }
+}
